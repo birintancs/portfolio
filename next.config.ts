055c46a0dd3a3d4feeb4eb57e01ff5e15a048d77
birintancs <76602620+birintancs@users.noreply.github.com>
2025-07-05 19:57:53 -0400

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  esling: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
