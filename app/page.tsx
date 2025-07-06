6f45610f91cb2fd3b0279b1522e2f43a2f9f8775
birintancs <76602620+birintancs@users.noreply.github.com>
2025-07-05 20:02:49 -0400

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Hero from "./ui/Hero"
 import Projects from "./ui/Projects"
-import Education from "./ui/Education"
-import Contact from "./ui/ContactMe"
 import Experience from "./ui/Experience"
 
 export default function Page() {
